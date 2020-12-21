dd79a0cb46885a9442acd1ce0837cf9d5ee26a98
yoji <ojimay@colabmix.co.jp>
2020-12-21 09:35:29 +0900

    css修正

--- app/src/sample/components/Aside.js
+++ app/src/sample/components/Aside.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { createStore } from "redux";
 
 export default function Top(props) {
-  <aside class="_1us6f63">
-    <a class="_bl1ufs">
-      <span class="_138jip5">Airbnbの新型コロナウイルスに対する取り組みの最新情報をご案内</span>
+  <aside className="_1us6f63">
+    <a className="_bl1ufs">
+      <span className="_138jip5">Airbnbの新型コロナウイルスに対する取り組みの最新情報をご案内</span>
     </a>
   </aside>
 
